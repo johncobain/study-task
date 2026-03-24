87ed82c96183295178d6e6fa71dcbc3cc8b5d96c
Andrey Gomes <andreygdsn@gmail.com>
2026-03-24 15:05:45 -0300

    initial commit

---
+++ studytaskServer/src/test/java/br/com/johncobain/studytaskServer/StudytaskServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.johncobain.studytaskServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudytaskServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
